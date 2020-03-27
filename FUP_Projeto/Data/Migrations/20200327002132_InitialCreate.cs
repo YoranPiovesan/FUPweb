07c31f4f2548d6373589ce20188db6f013bc2225
YoranPiovesan <yoran_piovesan@hotmail.com>
2020-03-27 20:40:33 -0300

    teste01

---
+++ FUP_Projeto/Data/Migrations/20200327002132_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FUP_Projeto.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 60, nullable: false),
+                    Cargo = table.Column<string>(maxLength: 60, nullable: false),
+                    Chefe = table.Column<string>(maxLength: 60, nullable: false),
+                    Telefone = table.Column<string>(nullable: true),
+                    CPF = table.Column<string>(maxLength: 11, nullable: false),
+                    Salario = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ FUP_Projeto/Data/MvcFuncionarioContext.cs
@@ -0,0 +1,19 @@
+﻿using FUP_Projeto.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FUP_Projeto.Data
+{
+    public class MvcFuncionarioContext : IdentityDbContext
+    {
+       public MvcFuncionarioContext(DbContextOptions<MvcFuncionarioContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Funcionario> Movie { get; set; }
+    }
+}
